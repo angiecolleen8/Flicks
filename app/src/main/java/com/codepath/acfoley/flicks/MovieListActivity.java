1f2812086bd5f88d3a0f37accfcb422a885b975f
Angela Foley <acfoley@fb.com>
2018-06-28 18:19:56 -0700

    Through video 3. Overview and movie title are displaying, though text formatting is incorrect.

--- app/src/main/java/com/codepath/acfoley/flicks/MovieListActivity.java
+++ app/src/main/java/com/codepath/acfoley/flicks/MovieListActivity.java
@@ -2,6 +2,8 @@ package com.codepath.acfoley.flicks;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -22,7 +24,7 @@ public class MovieListActivity extends AppCompatActivity {
 
     //constants
     //base url for API
-    public final static String API_BASE_URL = "https://api.themoviebd.org/3";
+    public final static String API_BASE_URL = "https://api.themoviedb.org/3";
     //parameter name for API key
     public final static String API_KEY_PARAM = "api_key";
     //tag for logging
@@ -33,6 +35,10 @@ public class MovieListActivity extends AppCompatActivity {
     String posterSize;
     //parsed list of currently playing movies
     ArrayList<Movie> movies;
+    //the recycler view
+    RecyclerView rvMovies;
+    //Adapter wired to recycler view
+    MovieAdapter adapter;
 
 
     //instance fields - only have values associated with instances of MovieListActivity
@@ -46,23 +52,48 @@ public class MovieListActivity extends AppCompatActivity {
         client = new AsyncHttpClient();
         //initialize ArrayList movies
         movies = new ArrayList<>();
+        //initialize adapter - movies cannot be reintialized after this point
+        adapter = new MovieAdapter(movies);
+
+        //resolve the recycler view and connect a layout manager and the adapter
+        rvMovies = (RecyclerView) findViewById(R.id.rvMovies);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this)); //'this' is reference to activity
+        rvMovies.setAdapter(adapter);
+
         //get configuration upon app creation
         getConfiguration();
     }
 
-
-
-    /* START HERE: EPISODE 3 5:15*/
     //get list of currently playing movies from api
     private void getNowPlaying() {
         //create url
-        String url = API_BASE_URL + "/movies/get-now-playing";
+        String url = API_BASE_URL + "/movie/now_playing";
         //set request params
         RequestParams params = new RequestParams();
         params.put(API_KEY_PARAM, getString(R.string.api_key)); //API key --required
         //execute GET request. expect JSON object response
         client.get(url, params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //load results into movie array
+                try {
+                    JSONArray results = response.getJSONArray("results");
+                    for (int i = 0; i < results.length(); i++) {
+                        Movie movie = new Movie(results.getJSONObject(i));
+                        movies.add(movie);
+                        //notify adapter that row was added
+                        adapter.notifyItemInserted(movies.size() -1);
+                    }
+                    Log.i(TAG, String.format("Loaded %s movies", results.length()));
+                } catch (JSONException e) {
+                    logError("Failed to parse now playing.", e, true);
+                }
+            }
 
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                logError("Failed to get data from now playing endpoint.", throwable, true);
+            }
         });
 
     }
@@ -87,6 +118,10 @@ public class MovieListActivity extends AppCompatActivity {
                     JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
                 //try to use option 3, else size 342
                     posterSize = posterSizeOptions.optString(3, "w342");
+                    Log.i(TAG, String.format("Loaded configuration imageBaseURL %s and posterSize %s", posterSize, imageBaseUrl));
+                    //get now playing movie list
+                    getNowPlaying(); //moved here so that getConfiguration has to execute BEFORE getNowPlaying
+
                 } catch (JSONException e) {
                     logError("Failed parsing configuration", e, true);
                 }
